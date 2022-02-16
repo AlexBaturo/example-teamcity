8b9286f83553d8390ff5d7d0c0fdb47cc33e6024
alex <alex@TeamCity>
2022-02-16 19:44:00 +0000

    TeamCity change in 'Java Project' project: runners of 'Test or deploy to Nexus' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -45,10 +45,6 @@ object Test : BuildType({
     steps {
         maven {
             name = "Build master and deploy to Nexus"
-
-            conditions {
-                equals("teamcity.build.branch", "master")
-            }
             goals = "clean deploy"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             userSettingsSelection = "settings.xml"
